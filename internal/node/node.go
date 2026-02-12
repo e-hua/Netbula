e1f3a8977f4eb7eb5f8b1b3a4b1e64a2470e2277
e-hua <eclipsehua@gmail.com>
2026-02-12 15:58:22 +0800

    Implement the skeleton for the Node struct

--- internal/node/node.go
+++ internal/node/node.go
@@ -1,1 +1,13 @@
 package node
+
+// The physical aspect of the Worker
+type Node struct {
+	Name string
+	Cores int 
+	Memory int 
+	MemoryAllocated int 
+	Disk int 
+	DiskAllocated int 
+	Role string
+	TaskCount int 
+}
