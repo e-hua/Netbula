9de1c657ab31b3f0446985f0ed6164a5f3c8fb53
e-hua <eclipsehua@gmail.com>
2026-02-14 10:45:30 +0800

    Add state machine logic to the Task package

--- internal/task/task.go
+++ internal/task/task.go
@@ -3,19 +3,48 @@ package task
 import (
 	"time"
 
-	"github.com/docker/go-connections/nat"
+	"github.com/e-hua/netbula/internal/docker"
 	"github.com/google/uuid"
+	"github.com/moby/moby/api/types/network"
 )
 
 type State int 
 
 const (
+	// Initial state for every task 
 	Pending State = iota // State = 0
+	// Once the manager has scheduled it onto a worker
 	Scheduled  // Scheduled = 1
+	// Once a worker successfully starts the task
 	Running  // ...
+	// Once the task completes its work 
 	Completed
+	// If task fails 
 	Failed
 )
+
+// Think of this as an adjacent list 
+// In the graph representing the state machine
+var stateTransitionMap = map[State][]State {
+	Pending: {Scheduled},
+	Scheduled: {Scheduled, Running, Failed},
+	Running: {Running, Completed, Failed},
+	Completed: {},
+	Failed: {},
+}
+
+func contains(states []State, state State) bool {
+	for _, s := range(states) {
+		if (s == state) {
+			return true
+		}
+	}
+	return false
+}
+
+func ValidStateTransition(src State, dest State) bool {
+	return contains(stateTransitionMap[src], dest)
+}
 
 // Acts as an abstraction for a container
 type Task struct {
@@ -24,15 +53,18 @@ type Task struct {
 	Name string 
 	State State
 	Image string 
+	Cpu float64
 	Memory int 
 	Disk int 
 
-	ExposedPorts nat.PortSet
+	ExposedPorts network.PortSet
 	PortBindings map[string]string
 	RestartPolicy string
 
 	StartTime time.Time
 	FinishTime time.Time
+
+	ContainerID string
 }
 
 type TaskEvent struct {
@@ -40,4 +72,25 @@ type TaskEvent struct {
 	TargetState State
 	Timestamp time.Time
 	Task Task
+}
+
+func NewConfig(task *Task) docker.Config {
+	return docker.Config{
+		Name: task.Name,
+
+		AttachStdin: true,
+		AttachStdout: true,
+		AttachStderr: true,
+
+		ExposedPorts: task.ExposedPorts,
+
+		Cmd: make([] string, 0),
+		Image: task.Image,
+
+		Cpu: task.Cpu,
+		Memory: task.Memory,
+		Disk: task.Disk,
+		Env: make([] string, 0),
+		RestartPolicy: task.RestartPolicy,
+	}
 }
