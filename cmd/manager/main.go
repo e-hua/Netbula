a150d7324937b72357b3ab6878113ac076233100
e-hua <eclipsehua@gmail.com>
2026-02-16 16:07:42 +0800

    Add manager API to application entry

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -9,18 +9,17 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/e-hua/netbula/internal/app/manager"
 	"github.com/e-hua/netbula/internal/app/worker"
 	"github.com/e-hua/netbula/internal/networks/security"
 	"github.com/e-hua/netbula/internal/networks/types"
-	"github.com/e-hua/netbula/internal/task"
 	"github.com/google/uuid"
 	"github.com/hashicorp/yamux"
 )
 
-const UpdateTasksPeriod = 15 * time.Second
 
 func createTlsListener(port string) net.Listener {
 	cert, token := security.GenerateManagerIdentity()
@@ -59,18 +58,28 @@ func connectAndCreateHttpClient(listener net.Listener) (*http.Client, error) {
 }
 
 func main() {
-	if (len(os.Args) < 2) {
+	if (len(os.Args) < 3) {
 		fmt.Fprintln(
 			os.Stderr, 
 			"Not enough arguments, program must be called with" +  
-			"go run main.go <port_number>");
+			"go run main.go <port_number_for_worker_connection> <port_number_for_manager_api>");
 		return;
+	}
+
+	managerApiPort, err := strconv.Atoi(os.Args[2])
+	if (err != nil) {
+		log.Fatalf("Invalid port number for manager API: %s", os.Args[2])
 	}
 
 	formattedPort := fmt.Sprintf(":%v", os.Args[1])
 	listener := createTlsListener(formattedPort)
 
 	newManager := manager.New(make([]uuid.UUID, 0));
+	managerApi := manager.Api{Manager: newManager, Port: managerApiPort}
+
+	go newManager.SendTasksForever()
+	go newManager.UpdateTasksForever()
+	go managerApi.Start()
 
 	for {
 		httpClient, err := connectAndCreateHttpClient(listener)
@@ -92,93 +101,6 @@ func main() {
 
 		workerStats := getWorkerStatus(httpClient)
 		printWorkerStatus(workerStats)
-
-		// Add and send demo taskevents
-		for i := range 3 {
-			t := task.Task{
-				ID: uuid.New(),
-				Name: fmt.Sprintf("test-container-%d", i),
-				State: task.Scheduled,
-				Image: "strm/helloworld-http",
-			}
-
-			te := task.TaskEvent{
-				ID: uuid.New(),
-				TargetState: task.Running,
-				Task: t,
-			}
-
-			newManager.AddTaskEvent(te)
-			newManager.SendWork()
-		}
-
-		go func() {
-			for {
-				fmt.Printf("[Manager] Updating tasks from %d workers\n", len(newManager.Workers))
-				newManager.UpdateTasks()
-				time.Sleep(UpdateTasksPeriod)
-			}
-		}()
-
-		time.Sleep(90 * time.Second)
-
-		for taskId := range(newManager.TaskMap) {
-			path := "http://worker/tasks/" + taskId.String()
-
-			req, err := http.NewRequest(http.MethodDelete, path , nil)
-			if (err != nil) {
-				panic(err)
-			}
-
-			httpClient.Do(req)
-		}
-
-		for { 
-			for _, t := range newManager.TaskMap { 
-				fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
-			}
-			time.Sleep(15 * time.Second)
-		}
-		/*
-		taskEvent := task.TaskEvent {
-			ID: uuid.New(),
-			TargetState: task.Running,
-			Task: task.Task { 
-				ID: uuid.New(),
-				Name: "test-chapter-5-1",
-				State: task.Scheduled,
-				Image: "strm/helloworld-http",
-			},
-		}
-		taskEventData, err := json.Marshal(taskEvent)
-		if (err != nil) {
-			panic(err)
-		}
-
-		// Start the task 
-		newTask := &task.Task{}
-		resp, _  = httpClientMap[workerInfo.Name].Post("http://worker/tasks", "application/json", bytes.NewBuffer(taskEventData))	
-		err = json.NewDecoder(resp.Body).Decode(newTask)
-		if (err != nil) {
-			panic(err)
-		}
-		fmt.Printf("New task: %v\n", newTask)
-
-		time.Sleep(30 * time.Second)
-
-		workerStats = getWorkerStatus(httpClientMap[workerInfo.Name])
-		printWorkerStatus(workerStats)
-
-		// Delete the task 
-		path := "http://worker/tasks/" + newTask.ID.String()
-
-		req, err := http.NewRequest(http.MethodDelete, path , nil)
-		if (err != nil) {
-			panic(err)
-		}
-		httpClientMap[workerInfo.Name].Do(req)
-		fmt.Println("Task deleted")
-		*/
 	}	
 }
 
