31cdb357307fb4d8d5808bccb268579cb6d5a14a
e-hua <eclipsehua@gmail.com>
2026-02-17 11:21:27 +0800

    Add roundRobin struct implementing the Scheduler interface

---
+++ internal/scheduler/roundRobin.go
@@ -0,0 +1,50 @@
+package scheduler
+
+import (
+	"github.com/e-hua/netbula/internal/node"
+	"github.com/e-hua/netbula/internal/task"
+)
+
+type RoundRobin struct {
+	Name string
+	LastWorkerIdx int 
+}
+
+func (r *RoundRobin) SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node {
+	return nodes
+}
+
+func (r *RoundRobin) Score(t task.Task, nodes []*node.Node) map[string]float64 {
+	nodeScores := make(map[string]float64)
+
+	newWorkerIdx := (r.LastWorkerIdx + 1) % len(nodes)
+	r.LastWorkerIdx = newWorkerIdx
+
+	for idx, node := range(nodes) {
+		if idx == newWorkerIdx {
+			nodeScores[node.Name] = 0.1
+		} else {
+			nodeScores[node.Name] = 1.0
+		}
+	}
+	return nodeScores
+}
+
+// Pick the node with lowest score 
+func (r *RoundRobin) Pick(scores map[string]float64, candidates []*node.Node) *node.Node {
+	var bestNode *node.Node 
+	var lowestScore float64
+
+	for idx, node := range(candidates) {
+		if (idx == 0)	{
+			bestNode = node
+			lowestScore = scores[node.Name]	
+		} else if (scores[node.Name] < lowestScore) {
+			bestNode = node
+			lowestScore = scores[node.Name]
+		}
+	}
+
+	return bestNode
+}
+
