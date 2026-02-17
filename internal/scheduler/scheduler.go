32a79ba2bf88b1d07708b8fdad2212f6decc442f
e-hua <eclipsehua@gmail.com>
2026-02-17 11:19:34 +0800

    Update the abstract methods in Scheduler interface

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,7 +1,13 @@
 package scheduler
 
+import (
+	"github.com/e-hua/netbula/internal/node"
+	"github.com/e-hua/netbula/internal/task"
+)
+
 type Scheduler interface {
-	SelectCandidiateNodes()
-	Score()
-	Pick()
+	SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node
+	Score(t task.Task, nodes []*node.Node) map[string]float64
+	Pick(scores map[string]float64, candidates []*node.Node) *node.Node
 }
+
