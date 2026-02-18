3217e917313371f6d64dec76f2a9640f78ed8131
e-hua <eclipsehua@gmail.com>
2026-02-18 21:49:09 +0800

    Change type signatures of methods of Scheduler to use uuid instead of string for maps

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -3,11 +3,12 @@ package scheduler
 import (
 	"github.com/e-hua/netbula/internal/node"
 	"github.com/e-hua/netbula/internal/task"
+	"github.com/google/uuid"
 )
 
 type Scheduler interface {
 	SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node
-	Score(t task.Task, nodes []*node.Node) map[string]float64
-	Pick(scores map[string]float64, candidates []*node.Node) *node.Node
+	Score(t task.Task, nodes []*node.Node) map[uuid.UUID] float64
+	Pick(scores map[uuid.UUID]float64, candidates []*node.Node) *node.Node
 }
 
