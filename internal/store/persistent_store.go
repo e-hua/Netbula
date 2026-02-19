f24e50ccdc7e0746122a0dcabd2fe753f8329be5
e-hua <eclipsehua@gmail.com>
2026-02-19 21:18:43 +0800

    Implement generic struct PersistentStore using bbolt

---
+++ internal/store/persistent_store.go
@@ -0,0 +1,117 @@
+package store
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+type PersistentStore[T any] struct {
+	Db *bolt.DB
+	DbFilePath string
+	FileMode os.FileMode
+	BucketName string	
+}
+
+func NewPersistentStore[T any](mode os.FileMode, dbFilePath string, bucketName string) (*PersistentStore[T], error){
+	db, err := bolt.Open(dbFilePath, mode, nil)
+	if (err != nil) {
+		return nil, err
+	}
+
+
+	// Need to create the bucket
+	persistentDb := &PersistentStore[T]{
+		Db: db,
+		DbFilePath: dbFilePath,
+		FileMode: mode,
+		BucketName: bucketName,
+	}
+
+	err = persistentDb.CreateBucket()
+
+	return persistentDb, err
+}
+
+func (p *PersistentStore[T]) Put(key string, value *T) error {
+	// A read operation 
+	return p.Db.Update(func (tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(p.BucketName))
+
+		valueInBytes, err := json.Marshal(value)
+		if (err != nil) {
+			return err
+		}
+
+		err = bucket.Put([]byte(key), valueInBytes)
+		return err
+	}) 
+}
+
+func (p *PersistentStore[T]) Get(key string) (*T, error) {
+	var val T
+
+	err := p.Db.View(func (tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(p.BucketName))	
+		valueInBytes := bucket.Get([]byte(key))
+
+		if (valueInBytes == nil) {
+			return fmt.Errorf("Key %v not found in the bucket", key)
+		}
+
+		return json.Unmarshal(valueInBytes, &val)
+	})
+
+	return &val, err 
+}
+
+func (p *PersistentStore[T]) List() ([]*T, error) {
+	valueCount, err := p.Count()
+	if (err != nil) {
+		return nil, err
+	}
+
+	var values []*T = make([]*T, 0, valueCount)
+
+	err = p.Db.View(func (tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(p.BucketName))
+
+		err := bucket.ForEach(func (k, valueInBytes []byte) error {
+			var value T
+			currErr := json.Unmarshal(valueInBytes, &value)
+			values = append(values, &value)
+
+			return currErr
+		})
+
+		return err
+	})
+
+	return values, err
+}
+
+func (p *PersistentStore[T]) Count() (int, error) {
+	keyCount := 0
+
+	// Read the number of key/value pairs in the bucket 
+	p.Db.View(func (tx *bolt.Tx) error {
+		keyCount = tx.Bucket([]byte(p.BucketName)).Stats().KeyN
+		return nil
+	})
+
+	return keyCount, nil
+}
+
+func (p *PersistentStore[T]) CreateBucket() error {
+	return p.Db.Update(func (tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(p.BucketName))
+		return err
+	})
+}
+
+func (p *PersistentStore[T]) Close() error {
+	return p.Db.Close()
+}
+
