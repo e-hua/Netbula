cfd3e472ffc44e80e41a27444976080f0212f645
e-hua <eclipsehua@gmail.com>
2026-02-20 22:34:28 +0800

    Add ManagerConfig type storing TLS token and certificate with ports

---
+++ internal/configs/manager_config.go
@@ -0,0 +1,26 @@
+package configs
+
+import (
+	"crypto/ed25519"
+	"crypto/tls"
+)
+
+type ManagerConfig struct {
+	WorkerConnectionPort int  
+	ServerApiPort int
+	// TlsCertificate tls.Certificate
+	TlsCertificateInBytes [][]byte
+	TlsPrivateKey []byte
+	// token: SHA-256 hash of certificate
+	TlsToken string
+}
+
+func NewManagerConfig(workerConnectionPort int, serverApiPort int, tlsCertificate tls.Certificate ,tlsToken string) *ManagerConfig {
+	return &ManagerConfig{
+		WorkerConnectionPort: workerConnectionPort,
+		ServerApiPort: serverApiPort,
+		TlsCertificateInBytes: tlsCertificate.Certificate,
+		TlsPrivateKey: tlsCertificate.PrivateKey.(ed25519.PrivateKey),
+		TlsToken: tlsToken,
+	} 
+}
